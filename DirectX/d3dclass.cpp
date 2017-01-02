aa687763a452fe50f3e778a9016949ea198f952e
Herve Jauffret <Herve Jauffret>
2017-01-02 16:54:57 +0100

    FIX nvidia bug with dx11 feature level

--- DirectX/d3dclass.cpp
+++ DirectX/d3dclass.cpp
@@ -206,7 +206,7 @@ bool D3DClass::Initialize(int screenWidth, int screenHeight, bool vsync, HWND hw
 	swapChainDesc.Flags = 0;
 
 	// Set the feature level to DirectX 11.
-	featureLevel = D3D_FEATURE_LEVEL_11_1;
+	featureLevel = D3D_FEATURE_LEVEL_11_0;
 	
 	// Create the swap chain, Direct3D device, and Direct3D device context.
 	result = D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, D3D11_CREATE_DEVICE_DEBUG, &featureLevel, 1,
