39a8e44d3eae07d257d709e4e96c66707a452215
Benjamin Fonty <benjamin.fonty@gmail.com>
2017-01-03 22:38:13 +0100

    Removed useless files

--- DirectX/applicationclass.cpp
+++ DirectX/applicationclass.cpp
@@ -91,13 +91,13 @@ bool ApplicationClass::Initialize(HINSTANCE hinstance, HWND hwnd, int screenWidt
 	}
 
 	// Load textures into the texture manager.
-	result = m_TextureManager->LoadTexture(m_Direct3D->GetDevice(), m_Direct3D->GetDeviceContext(), "../Engine/data/textures/test.tga", 0);
+	result = m_TextureManager->LoadTexture(m_Direct3D->GetDevice(), m_Direct3D->GetDeviceContext(), "../textures/test.tga", 0);
 	if (!result)
 	{
 		return false;
 	}
 
-	result = m_TextureManager->LoadTexture(m_Direct3D->GetDevice(), m_Direct3D->GetDeviceContext(), "../Engine/data/textures/dirt01d.tga", 1);
+	result = m_TextureManager->LoadTexture(m_Direct3D->GetDevice(), m_Direct3D->GetDeviceContext(), "../textures/dirt01d.tga", 1);
 	if (!result)
 	{
 		return false;
